82e27ac94cd7bb9a9126c03231e6d5413b7f2ae1
himanshu0422 <himanshumittal035@gmail.com>
2024-12-30 12:15:05 +0530

    api setup

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,34 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './common/prisma/prisma.module';
+import { UsersModule } from './models/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true,
+      fieldResolverEnhancers: ['guards'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+    }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Parknest')
+    .setDescription(
+      `The Parknest API.
+      <h2>Looking for the graphql api?</h2>
+      Go to <a href="/graphql" target="_blank">/graphql</a>.
+      Or,
+      You might also need to use the <a target="_blank" href="https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:3000/graphql&document=query users{users{ uid }}
+      ">Apollo explorer</a> for a greater experience.
+      `,
+    )
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
