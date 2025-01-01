745db771ee2b61122a6a1dcc1e4fff52289d0ce2
himanshu0422 <himanshumittal035@gmail.com>
2025-01-01 13:24:47 +0530

    network package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
 import { add } from "@parknest/sample-package";
+import { useQuery } from "@apollo/client";
+import { CompaniesDocument } from "@parknest/network/src/gql/generated";
 
 export default function Home() {
-  return <main>Hlo {add(1, 100)}</main>;
+  const { data } = useQuery(CompaniesDocument);
+
+  return (
+    <main>
+      Hello {add(343, 3)}
+      <div>
+        {data?.companies.map((company) => (
+          <div className="p-4 rounded" key={company.id}>
+            <div>{company.displayName}</div>
+            <div>{company.id}</div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
