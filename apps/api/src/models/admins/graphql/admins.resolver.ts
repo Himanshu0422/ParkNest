7e28886d2e186b4267f2e03edfdc94b5716eba4c
himanshu0422 <himanshumittal035@gmail.com>
2025-01-01 13:23:47 +0530

    resolve fields

--- apps/api/src/models/admins/graphql/admins.resolver.ts
+++ apps/api/src/models/admins/graphql/admins.resolver.ts
@@ -1,13 +1,22 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
-import { AdminsService } from './admins.service';
-import { Admin } from './entity/admin.entity';
-import { FindManyAdminArgs, FindUniqueAdminArgs } from './dtos/find.args';
-import { CreateAdminInput } from './dtos/create-admin.input';
-import { UpdateAdminInput } from './dtos/update-admin.input';
-import { checkRowLevelPermission } from 'src/common/auth/util';
-import { GetUserType } from 'src/common/types';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { AllowAuthenticated, GetUser } from 'src/common/auth/auth.decorator';
+import { checkRowLevelPermission } from 'src/common/auth/util';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { GetUserType } from 'src/common/types';
+import { User } from 'src/models/users/graphql/entity/user.entity';
+import { Verification } from 'src/models/verifications/graphql/entity/verification.entity';
+import { AdminsService } from './admins.service';
+import { CreateAdminInput } from './dtos/create-admin.input';
+import { FindManyAdminArgs, FindUniqueAdminArgs } from './dtos/find.args';
+import { UpdateAdminInput } from './dtos/update-admin.input';
+import { Admin } from './entity/admin.entity';
 
 @Resolver(() => Admin)
 export class AdminsResolver {
@@ -60,5 +69,22 @@ export class AdminsResolver {
     const admin = await this.prisma.admin.findUnique(args);
     checkRowLevelPermission(user, admin.uid);
     return this.adminsService.remove(args);
+  }
+
+  @ResolveField(() => User, { nullable: true })
+  user(@Parent() admin: Admin) {
+    return this.prisma.user.findUnique({ where: { uid: admin.uid } });
+  }
+  @ResolveField(() => [Verification])
+  verifications(@Parent() parent: Admin) {
+    return this.prisma.verification.findMany({
+      where: { adminId: parent.uid },
+    });
+  }
+  @ResolveField(() => Number)
+  async verificationsCount(@Parent() parent: Admin) {
+    return this.prisma.verification.count({
+      where: { adminId: parent.uid },
+    });
   }
 }
