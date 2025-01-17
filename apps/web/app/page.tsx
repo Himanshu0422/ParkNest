985015edc5ba35a9a8cc96736ad62cf078b21152
himanshu0422 <himanshumittal035@gmail.com>
2025-01-17 22:01:17 +0530

    showing garage list

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 
 import { useQuery } from "@apollo/client";
-import { CompaniesDocument } from "@parknest/network/src/gql/generated";
+import {
+  CompaniesDocument,
+  SearchGaragesDocument,
+} from "@parknest/network/src/gql/generated";
 
 export default function Home() {
   const { data } = useQuery(CompaniesDocument);
+
+  const { data: garages } = useQuery(SearchGaragesDocument, {
+    variables: {
+      dateFilter: { end: "2024-12-14", start: "2024-12-04" },
+      locationFilter: {
+        ne_lat: 1,
+        ne_lng: 1,
+        sw_lat: -1,
+        sw_lng: -1,
+      },
+    },
+  });
 
   return (
     <main className="">
@@ -14,6 +29,11 @@ export default function Home() {
             <div>{company.displayName}</div>
             <div>{company.id}</div>
           </div>
+        ))}
+      </div>
+      <div>
+        {garages?.searchGarages.map((garage) => (
+          <pre key={garage.id}>{JSON.stringify(garage, null, 2)}</pre>
         ))}
       </div>
     </main>
